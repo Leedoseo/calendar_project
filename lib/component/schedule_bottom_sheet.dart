e89cafed8b175285b4a668056ce156fcb660ca09
Leedoseo <okok0840@naver.com>
2025-07-30 18:28:49 +0900

    ScheduleBottomSheet에 CustomTextField반영 후 UI조정

--- lib/component/schedule_bottom_sheet.dart
+++ lib/component/schedule_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:calendar_scheduler/component/custom_text_field.dart';
+import 'package:calendar_scheduler/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class ScheduleBottomSheet extends StatefulWidget {
@@ -16,10 +17,51 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
         child: Container(
           height: MediaQuery.of(context).size.height / 2,
           color: Colors.white,
-          child: CustomTextField(
-            label: "시작 시간",
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: CustomTextField(
+                        label: "시작 시간",
+                        isTime: true,
+                      ),
+                    ),
+                    const SizedBox(width: 16.0),
+                    Expanded(
+                      child: CustomTextField(
+                        label: "종료 시간",
+                        isTime: true,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 8.0),
+                Expanded(
+                  child: CustomTextField(
+                    label: "내용",
+                    isTime: false,
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton( // 저장 버튼
+                    onPressed: onSavePressed,
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: PRIMARY_COLOR,
+                    ),
+                    child: Text("저장"),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
     );
+  }
+
+  void onSavePressed(){
   }
 }
