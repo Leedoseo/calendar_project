3b6de4358546f5cd77420d461c92a950883fe20f
Leedoseo <okok0840@naver.com>
2025-08-27 21:25:58 +0900

    파이어베이스 설정 추가

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,15 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:get_it/get_it.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:calendar_scheduler/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // 플러터 프레임워크가 준비될 때까지 대기
+
+  await Firebase.initializeApp( // 파이어베이스 프로젝트 설정 함수
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await initializeDateFormatting(); // intl 패키지 초기화(다국어화)
 
